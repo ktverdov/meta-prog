0192adb4e2d6e4345d10be080fd4ec1aae9909a0
Tverdov Konstantin <ktverdov@inbox.ru>
2018-11-27 12:49:24 +0300

    add test typelist

--- task-2/main.cpp
+++ task-2/main.cpp
@@ -2,65 +2,62 @@
 #include "TypeList.h"
 #include "GenFibonacciHierarchy.h"
 
-
 //testing typelist function (GetBeforeIndex, GetAfterIndex) 
 //needed for GenFibonacciHierarchy
-void test_typelist() {
-	// using type_1 = TypeList<int, char, double, float>;
-	// using type_2 = TypeList<>;
-	// using type_3 = TypeList<double>;
+namespace test_typelist
+{
+	using type_1 = TypeList<int, char, double, float>;
+	using type_2 = TypeList<>;
 
-	// using type_1_first = GetBeforeIndex<type_3, 0>::result;
-	// using type_1_second = GetAfterIndex<type_1, 2>::result;
 
-	// static_assert( std::is_same<type_1, TypeList<int, char, double, float> >{}, "!");
+	using temp_0 = GetBeforeIndex<type_1, 0>::result;
+	static_assert( std::is_same<temp_0, TypeList<> >{}, "!");
+
+	using temp_1 = GetBeforeIndex<type_1, 3>::result;
+	static_assert( std::is_same<temp_1, TypeList<int, char, double> >{}, "!");
+
+	using temp_2 = GetBeforeIndex<type_1, 5>::result;
+	static_assert( std::is_same<temp_2, TypeList<int, char, double, float> >{}, "!");
+
+	using temp_3 = GetBeforeIndex<type_2, 2>::result;
+	static_assert( std::is_same<temp_3, TypeList<> >{}, "!");
+
+	//---------
+
+	using temp_4 = GetAfterIndex<type_1, 2>::result;
+	static_assert( std::is_same<temp_4, TypeList<double, float> >{}, "!");
+
+	using temp_5 = GetAfterIndex<type_1, 4>::result;
+	static_assert( std::is_same<temp_5, TypeList<> >{}, "!");
+
+	using temp_6 = GetAfterIndex<type_1, 0>::result;
+	static_assert( std::is_same<temp_6, TypeList<int, char, double, float> >{}, "!");
 }
+
 
 template <class T, class Base>
 class Handler: public Base
 {
 public:
-	virtual void OnEvent(T& obj){
-		std::cout<<sizeof(T)<<std::endl;
-	}
+	virtual void OnEvent(T& obj) {}
 };
 
-class A{
-	int val;
-public:
-	int f1();
+// using hierarchy =  GenFibonacciHierarchy<TypeList<A, B, C, B, C, A, A, B, C>, Handler>;
+// using hierarchy =  GenFibonacciHierarchy<TypeList<A, B, C>, Handler>;
+
+
+template <typename T, typename Base>
+class EventHandler : public Base {
+	public:
+		virtual void OnEvent(T& obj) = 0;
 };
 
-class B{
-	char val;
-public:
-	int f2();
-};
+using linear_hierarchy = GenFibonacciHierarchy<TypeList<int, char, double>, EventHandler>;
 
-class C{
-	double val;
-public:
-	int f3();
-};
+int main() {	
 
-using hierarchy =  GenFibonacciHierarchy<TypeList<A, B, C, B, C, A, A, B, C>, Handler>;
-
-
-int main() {
-	test_typelist();	
+	linear_hierarchy test_obj;
 
 	return 0;
 }
 
-
-
-// template <typename T>
-// struct Holder {
-// 	T value_;
-// };
-
-// template <typename T, typename Base>
-// class EventHandler : public Base {
-// 	public:
-// 		virtual void OnEvent(const T&) = 0;
-// };
